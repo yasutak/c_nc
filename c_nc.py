1cb830c4528c69cdecb373ac8ef96223153d1c63
Yasutaka Odo <yasutakaodo@gmail.com>
2022-06-02 22:30:46 +0900

    working on calculate c-value funciton

--- c_nc.py
+++ c_nc.py
@@ -1,7 +1,8 @@
 import mypy
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 import Mykytea
 import re
+import numpy as np
 
 def get_morphological_labels(corpus: List[str]) -> List[List[Tuple[str, str]]]:
     """
@@ -22,21 +23,69 @@ def get_morphological_labels(corpus: List[str]) -> List[List[Tuple[str, str]]]:
     
     return lst
 
-def japanese_filter_regex(word_with_tags: List[List[str], List[str]]) -> bool:
+def japanese_filter_regex(tags: List[str]) -> bool:
     """
     Returns whether a word follows termilogy patters in Japanese.
 
     Args:
-        word_with_tags: A list of word and its tags.
+        words: A list of word and its tags.
+        tags: A list of tags.
     Returns:
         Whether the word is (probably) a terminology.
     """
+    tags_concat = "".join(tags)
 
-    return re.match(r"[\u3040-\u309F\u30A0-\u30FF\u31F0-\u31FF]", word) is not None
+    # filter1: Noun{2,}
+    # filter2: (Prefix | Adv) (Noun | Adj | Suffix)+ Noun+
+    # filter3: Prefix Noun+ Suffix
 
-def calculate_c_value(corpus: List[str]) -> List[float]:
-    pass
+    re_filter = re.compile(u"^(名詞){2,}$|(^((接頭辞)|(形容詞))((名詞)|(副詞)|(接尾辞))+(名詞)+)$|^((接頭辞)(名詞)+(接尾辞))$") # unicode
+
+    return re_filter.match(tags_concat) is not None # there is a match
+
+
+def build_frequency_table(corpus: List[str]) -> Dict[str, int]:
+    """
+    Returns a frequency table of the corpus.
+
+    Args:
+        corpus: A list of sentences.
+    Returns:
+        A frequency table of the corpus.
+    """
+    freq_table = {}
+    for sentence in corpus:
+        for word in sentence:
+            if word in freq_table:
+                freq_table[word] += 1
+            else:
+                freq_table[word] = 1
+    return freq_table
+
+def calculate_c_value(words: List[str], nested: bool) -> List[float]:
+    """
+    Returns the C value for a candidate terminoloy.
+
+    Args:
+        word: a candidate terminoloy in more than one word.
+        nested: Whether the word is nested.
+    Returns:
+        The C value for a word.
+    """
+    if not nested:
+        return np.log2(len(words))
+        
+    else:
+        
+    
+
 
 if __name__ == '__main__':
-    s = ["今日はいい天気です。", "1999年"]
-    print(conduct_morphological_analysis(s))
+    s = ["仮想関数", "擬似乱数系列", "非同期通信", "全二重接続", "再初期化", "未定義型"]
+    print(get_morphological_labels(s))
+    #assert get_morphological_labels(s) == [[['今日', '名詞']], [['は', '助詞']], [['い', '形容詞']], [['い', '語尾']], [['天気', '名詞']], [['で', '助動詞']], [['す', '語尾']], [['。', '補助記号']], [['1999', '名詞']], [['年', '名詞']]]
+    assert japanese_filter_regex(["名詞", "名詞"])
+    assert japanese_filter_regex(["名詞", "名詞"])
+    assert japanese_filter_regex(["接頭辞", "名詞", "名詞"])
+    assert japanese_filter_regex(["接頭辞", "名詞", "接尾辞"])
+    assert japanese_filter_regex(["接頭辞", "名詞", "名詞", "接尾辞"])
